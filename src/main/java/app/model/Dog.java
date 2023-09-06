183e6639d0a3c890d703049766ed13da3e88fb25
Sergei Kadushkin <blackoverlord.inc@mail.ru>
2023-09-06 11:58:09 +0300

    init

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -3,13 +3,13 @@ package app.model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component("catBean")
-public class Cat extends Animal {
+@Component("dogBean")
+public class Dog extends Animal {
     @Autowired
-    private Cat cat;
+    private Dog dog;
 
-    @Override
     public String toString() {
-        return "Im a Cat";
+        return "I am a Dog!";
     }
+
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,12 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("timerBean")
 public class Timer {
+    @Autowired
+    private Timer timer;
 
     private Long nanoTime = System.nanoTime();
 
